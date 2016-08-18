1627fb2e62fe1fb590942d2f029dca5e07b56ec2
rsgc-peterson-e <ethan@petetech.net>
2016-08-18 15:51:29 -0400

    added constructors to schedule class

--- What's The Day iOS/What's The Day iOS/Schedule.swift
+++ What's The Day iOS/What's The Day iOS/Schedule.swift
@@ -9,16 +9,20 @@
 import Foundation
 
 class Schedule {
-    var p1 : String
-    var p2 : String
-    var p3 : String
-    var p4 : String
+    var p1 : String!
+    var p2 : String!
+    var p3 : String!
+    var p4 : String!
     var p1Room : String!
     var p2Room : String!
     var p3Room : String!
     var p4Room : String!
     
-    init() {
+    init(p1Class : String, p2Class : String, p3Class : String, p4Class : String) { // constructor for main schedule
     
+    }
+    
+    init(roomP1 : String, roomP2 : String, roomP3 : String, roomP4 : String) { // constructor for adding the room numbers
+        
     }
 }
